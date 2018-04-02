40aa58f37dade2b5946fb58cd0113ad1b7565c71
yzp951 <1175686648@qq.com>
2018-04-02 14:38:15 +0800

    视频播放

--- Video/MainPage.xaml.cs
+++ Video/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,28 @@ namespace Video
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        async private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".mp3");
+            openPicker.FileTypeFilter.Add(".mp4");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                mediaPlayer.SetSource(stream, file.ContentType);
+                OutputTextBlock.Text = "fileName: " + file.Name;
+                mediaPlayer.Play();
+            }
+            else
+            {
+                OutputTextBlock.Text = "cancelled.";
+            }
         }
     }
 }
